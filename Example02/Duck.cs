e37f0a82609d96d69d3fedf4417a6ec1a595fa51
newhand2017 <chivalry19850@gmail.com>
2017-03-28 21:59:25 +0800

    策略模式<一>

---
+++ Example02/Duck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    public class Duck
+    {
+        public string Name { get; set; }
+
+        //public IFlyBehavior FlyBehavior;
+        private IFlyBehavior FlyBehavior;
+
+        public void Fly()
+        {
+            if (FlyBehavior == null)
+                Console.WriteLine("我不會飛。");
+            else
+                FlyBehavior.Fly();
+        }
+
+        //裝上飛行動作
+        public void Equip(IFlyBehavior iterm)
+        {
+            this.FlyBehavior = iterm;
+        }
+    }
+}
---
+++ Example02/老鷹果實.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    public class 老鷹果實 : 飛行系的果實
+    {
+        public override void Fly()
+        {
+            base.Fly();
+            Console.WriteLine("我用老鷹果實");
+        }
+    }
+}
